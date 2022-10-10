3a386ae968c3f986c78646d76b8b3d02af5ac3f6
Tiina Karppinen <karppinentj@gmail.com>
2022-10-10 23:48:15 +0300

    Valmis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Home from './components/home';
+import About from './components/about';
+import Contact from './components/contact';
+import NotFound from './components/notfound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+      <div className="App">
+        
+        <header className="App-header">
+          <h1>Welcome to React Router</h1>
+        </header>
+
+        <Link to="/">Home</Link>{' '}
+        <Link to="/about">About</Link>{' '}
+        <Link to="/contact">Contact</Link>{' '}
+        <Routes>
+          <Route path="/"element={<Home />} />
+          <Route path="/about"element={<About />} />
+          <Route path="/contact"element={<Contact />} />
+          <Route path="*"element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
